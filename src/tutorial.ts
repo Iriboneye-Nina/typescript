7a71a16584a70228bd60fc098545de9a3a8c70db
Iriboneye-Nina <ninairiboneye@gmail.com>
2024-08-12 15:00:20 +0200

    interface-exercises

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,4 +1,15 @@
 //Type Annotations:
 
-let name: string = "john";
-let age: number = 30;
+// let name: string = "john";
+// console.log('string')
+// let age: number = 30;
+// console.log('number')
+
+//Interfaces:
+ interface personal {
+    name: string,
+     age: number,
+ }
+ 
+
+
